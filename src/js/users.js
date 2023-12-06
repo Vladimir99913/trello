4424c181ff6b91dbf36acf0ed4e56679df4464bd
Vladimir <28butterfly10@gmail.com>
2023-12-06 18:45:43 +0300

    init(users.js): add function for to get users API

---
+++ src/js/users.js
@@ -0,0 +1,12 @@
+async function getUser(select) {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const user = await response.json();
+  user.forEach((element) => {
+    let optionAddElement = document.createElement('option');
+    optionAddElement.value = element.name;
+    optionAddElement.innerHTML = element.name;
+    select.append(optionAddElement);
+  });
+  console.log(user);
+}
+export { getUser };
