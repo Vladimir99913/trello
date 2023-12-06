cc2394606b9afcb83f8f359a9e4a113efe10a59d
Vladimir <28butterfly10@gmail.com>
2023-12-06 18:40:17 +0300

    init(localStorage.js): add functions for working with localStorage

---
+++ src/js/localStorage.js
@@ -0,0 +1,18 @@
+import { render } from './render.js';
+import { todos } from './handlers.js';
+
+function setData() {
+  localStorage.setItem('todos', JSON.stringify(todos));
+  console.log(todos);
+}
+function getData() {
+  const todos = localStorage.getItem('todos');
+  return todos ? JSON.parse(todos) : [];
+}
+
+if (localStorage.getItem('todos')) {
+  render();
+  getCount();
+}
+
+export { setData, getData };
